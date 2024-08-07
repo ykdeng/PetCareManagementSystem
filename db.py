7dab1bb2a6380b601f6ccd403d1bfcb2d3f6d80d
ykdeng <33257242+ykdeng@users.noreply.github.com>
2024-08-07 13:21:44 +0300

    Created file db.py

---
+++ db.py
@@ -0,0 +1,24 @@
+from sqlalchemy import create_engine, MetaData
+from sqlalchemy.orm import scoped_session, sessionmaker
+from sqlalchemy.ext.declarative import declarative_base
+import os
+from dotenv import load_dotenv
+
+load_dotenv()
+
+DATABASE_URL = os.getenv("DATABASE_URL", "sqlite:///example.db")
+
+engine = create_engine(DATABASE_URL, connect_args={"check_same_thread": False} if DATABASE_URL.startswith("sqlite://") else {})
+db_session = scoped_session(sessionmaker(autocommit=False, autoflush=False, bind=engine))
+
+Base = declarative_base()
+Base.query = db_session.query_property()
+
+def init_db():
+    Base.metadata.create_all(bind=engine)
+
+def get_db_session():
+    return db_session
+
+def close_db_session(exception=None):
+    db_session.remove()
