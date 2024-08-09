6d6cb60a15979d11b5ca322a8594a14f4704bb07
ykdeng <33257242+ykdeng@users.noreply.github.com>
2024-08-09 05:58:12 +0300

    advance the readability

--- db.py
+++ db.py
@@ -1,15 +1,25 @@
+import os
 from sqlalchemy import create_engine, MetaData
 from sqlalchemy.orm import scoped_session, sessionmaker
 from sqlalchemy.ext.declarative import declarative_base
-import os
 from dotenv import load_dotenv
 
 load_dotenv()
 
 DATABASE_URL = os.getenv("DATABASE_URL", "sqlite:///example.db")
 
-engine = create_engine(DATABASE_URL, connect_args={"check_same_thread": False} if DATABASE_URL.startswith("sqlite://") else {})
-db_session = scoped_session(sessionmaker(autocommit=False, autoflush=False, bind=engine))
+engine = create_engine(
+    DATABASE_URL, 
+    connect_args={"check_same_thread": False} if DATABASE_URL.startswith("sqlite://") else {}
+)
+
+db_session = scoped_session(
+    sessionmaker(
+        autocommit=False, 
+        autoflush=False, 
+        bind=engine
+    )
+)
 
 Base = declarative_base()
 Base.query = db_session.query_property()
