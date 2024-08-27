f886f38f85cdb11b60d5aca0f7c045e4d8ad7a29
ykdeng <33257242+ykdeng@users.noreply.github.com>
2024-08-27 15:20:25 +0300

    Add user.py

---
+++ models/user.py
@@ -0,0 +1,28 @@
+from flask_sqlalchemy import SQLAlchemy
+from os import environ
+
+db = SQLAlchemy()
+
+class User(db.Model):
+    __tablename__ = 'users'
+    id = db.Column(db.Integer, primary_key=True)
+    username = db.Column(db.String(80), unique=True, nullable=False)
+    email = db.Column(db.String(120), unique=True, nullable=False)
+    password_hash = db.Column(db.String(128))
+    posts = db.relationship('Post', backref='author', lazy=True)
+
+    def __repr__(self):
+        return '<User %r>' % self.username
+
+class Post(db.Model):
+    __tablename__ = 'posts'
+    id = db.Column(db.Integer, primary_key=True)
+    title = db.Column(db.String(100), nullable=False)
+    body = db.Column(db.Text, nullable=False)
+    user_id = db.Column(db.Integer, db.ForeignKey('users.id'), nullable=False)
+
+    def __repr__(self):
+        return '<Post %r>' % self.title
+
+app.config['SQLALCHEMY_DATABASE_URI'] = environ.get('DATABASE_URL')
+app.config['SECRET_KEY'] = environ.get('SECRET_KEY')
